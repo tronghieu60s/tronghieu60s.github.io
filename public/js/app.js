88adbe1940ea0f6cf72e2257de4407802f2873a5
Trọng Hiếu <tronghieu60s@gmail.com>
2021-04-03 11:33:15 +0700

    update style

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+function getQueryStringValue(key) {
+  return decodeURIComponent(
+    window.location.search.replace(
+      new RegExp(
+        "^(?:.*[&\\?]" +
+          encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") +
+          "(?:\\=([^&]*))?)?.*$",
+        "i"
+      ),
+      "$1"
+    )
+  );
+}
